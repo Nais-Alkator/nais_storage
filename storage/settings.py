35719d66171231b8eb394d8abc1de63006c43efe
Nikita <39242713+Nais-Alkator@users.noreply.github.com>
2022-07-10 00:39:22 +0300

    Update settings.py

--- storage/settings.py
+++ storage/settings.py
@@ -108,11 +108,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 PHONENUMBER_DB_FORMAT = 'NATIONAL'
 
 PHONENUMBER_DEFAULT_REGION = 'BY'
-
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.ScryptPasswordHasher',
-]
